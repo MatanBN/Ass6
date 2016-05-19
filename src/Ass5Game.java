a7476f958c2d27db69f7f41690fdd207dab6bdc4
Matan <matanbn22@gmail.com>
2016-05-19 21:04:51 +0300

    Update

--- src/Ass5Game.java
+++ src/Ass5Game.java
@@ -6,12 +6,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * The Ass3Game initializes and start the game.
+ * The Ass5Game initializes and start the game.
  * @author Matan Ben Noach Nir Ben Shalom
  * @version 1.0 9 April 2016
  */
 
-public class Ass3Game {
+public class Ass5Game {
     /**
      * The main creates the game object, initializes it and runs it.
      * @param args the input from command line.
